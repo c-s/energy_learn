c22ab9dc3373b4200acf2a0bf40b02d992cb71af
c-s <cs.p@outlook.com>
2015-05-16 15:57:49 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::io;
 
-pub fn read_mnist_images(imagefile: &str, labelfile: &str) -> io::Result<u8> { //-> io::Result<&'static [u8]> {
+const IMAGE_SIZE: usize = 28 * 28;
+const IMAGE_NUM_ROWS: usize = 28;
+const IMAGE_NUM_COLS: usize = 28;
+
+pub fn read_mnist_images(imagefile: &str, labelfile: &str) -> io::Result<Vec<MnistImage>> { //-> io::Result<&'static [u8]> {
     let mut image_buf = io::BufReader::new(try!(File::open(imagefile)));
     let mut label_buf = io::BufReader::new(try!(File::open(labelfile)));
 
@@ -12,27 +16,44 @@ pub fn read_mnist_images(imagefile: &str, labelfile: &str) -> io::Result<u8> { /
     let mut u32buf1: [u8; 4] = [0u8; 4];
     image_buf.read(&mut u32buf0[..]);
     label_buf.read(&mut u32buf1[..]);
-    if u32buf0 != [0u8, 0, 8, 1] || u32buf1 != [0u8, 0, 8, 3] {
+    if u32buf0 != [0u8, 0, 8, 3] || u32buf1 != [0u8, 0, 8, 1] {
         return Err(io::Error::new(io::ErrorKind::Other, "read_mnist_images: magic number does not match."))
     }
-    file.read(&mut u32buf0);
-    // i don't know how to get int out of 4 bytes using an rust library.
-    let num_image_items = ((u32buf0[0] as u32) << 24) +
-                    ((u32buf0[1] as u32) << 16) +
-                    ((u32buf0[2] as u32) << 8) +
-                    (u32buf0[3] as u32);
-    file.read(&mut u32buf0);
-    // i don't know how to get int out of 4 bytes using an rust library.
-    let num_label_items = ((u32buf1[0] as u32) << 24) +
-                    ((u32buf1[1] as u32) << 16) +
-                    ((u32buf1[2] as u32) << 8) +
-                    (u32buf1[3] as u32);
-    if num_image_items != num_label_ites {
-        return Err(io::Error::new(io::ErrorKind::Other, "the number of the images do not match with those of the labels."));
+    image_buf.read(&mut u32buf0);
+    // i don't know how to get int out of 4 bytes using a rust library.
+    let num_image_items = bytes_to_u32(&u32buf0);
+    label_buf.read(&mut u32buf1);
+    // i don't know how to get int out of 4 bytes using a rust library.
+    let num_label_items = bytes_to_u32(&u32buf1);
+    if num_image_items != num_label_items {
+        return Err(io::Error::new(io::ErrorKind::Other,
+                    format!("the number of the images {} do not match with those of the labels {}.",
+                            num_image_items, num_label_items)));
     }
-    println!("num_items {}", num_items);
-    
-    Ok(12u8)
+    println!("reading {} image and label pairs from {} and {}...",
+                num_image_items, imagefile, labelfile);
+    image_buf.read(&mut u32buf0[..]);
+    let row_dimension = bytes_to_u32(&u32buf0);
+    if row_dimension != 28 {
+        return Err(io::Error::new(io::ErrorKind::Other,
+                    format!("the row dimension {} is not 28.", row_dimension)))
+    }
+    image_buf.read(&mut u32buf1[..]);
+    let col_dimension = bytes_to_u32(&u32buf1);
+    if col_dimension != 28 {
+        return Err(io::Error::new(io::ErrorKind::Other,
+                    format!("the col dimension {} is not 28.", col_dimension)))
+    }
+    //let mut images: Vec<MnistImage> = Vec::with_capacity(num_image_items as usize);
+    let mut images = vec![MnistImage { data: [0u8; IMAGE_SIZE], label: 0u8 }; num_image_items as usize];
+    for i in 0..num_image_items {
+        let one_image = &mut images[i as usize]; // IMAGE_SIZE * i..IMAGE_SIZE * (i+1)];
+        image_buf.read(&mut one_image.data);
+        let mut one_label = [0u8];
+        label_buf.read(&mut one_label);
+        one_image.label = one_label[0];
+    }
+    Ok(images)
 /*
         for x in &magic_buf {
             println!("x: {}", x);
@@ -59,7 +80,35 @@ pub fn read_mnist_images(imagefile: &str, labelfile: &str) -> io::Result<u8> { /
 //    }
 //}
 
-struct MnistImage {
-    data: [u8; 28 * 28],
+pub struct MnistImage {
+    data: [u8; IMAGE_SIZE],
     label: u8
 }
+
+impl fmt::Debug for MnistImage {
+    fn fmt(&self, _ : &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        println!("MnistImage: data size {}, label {}", IMAGE_SIZE, self.label);
+        Ok(())
+    }
+}
+
+impl Clone for MnistImage {
+    fn clone(&self) -> Self {
+        MnistImage {
+            data: *(&self.data).clone(),
+            label: self.label
+        }
+    }
+    fn clone_from(&mut self, source: &Self) {
+        self.data = *(&source.data).clone();
+        self.label = source.label;
+    }
+}
+
+fn bytes_to_u32(bytes: &[u8; 4]) -> u32 {
+    ((bytes[0] as u32) << 24) +
+        ((bytes[1] as u32) << 16) +
+        ((bytes[2] as u32) << 8) +
+        (bytes[3] as u32)
+}
+    
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate energy_learn;
 use energy_learn::*;
 
 fn main() {
-    let m = read_mnist_images("/Users/changsoonpark/data/mnist/train-labels-idx1-ubyte"); 
+    let m = read_mnist_images("/Users/chang-soon/data/mnist/train-images-idx3-ubyte",
+                              "/Users/chang-soon/data/mnist/train-labels-idx1-ubyte");
     match m {
-        Ok(x) => println!("okay {}", x),
+        Ok(x) => println!("okay {:?}", x),
         Err(e) => println!("error {}", e)
     }
     println!("hi");
